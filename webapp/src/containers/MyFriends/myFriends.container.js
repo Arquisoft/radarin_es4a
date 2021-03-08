ea5d24e6105bdc0a1d106b81fe2b8fad839fd4b0
Israel <israelmnrg1b@gmail.com>
2021-03-08 18:08:04 +0100

    falta implementación del addFriends

--- webapp/src/containers/MyFriends/myFriends.container.js
+++ webapp/src/containers/MyFriends/myFriends.container.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import { List } from "@solid/react";
-import {FriendCardComponent} from '../../components/Card/friendCard';
-
+import { Button, FormControl, Container} from "react-bootstrap";
+import { addFriend } from './friends.service';
 class MyFriends extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        enteredWebId: "",
+        };
+    }
+
+    updateFriendWebId = (evt) => { this.setState( {enteredWebId: evt.target.value}); };
 
 
     render(){
         return (
+            <Container>
             <div>
+                <h1>Añadir amigo:</h1>
+                <FormControl onChange={this.updateFriendWebId} type="webID" placeholder="Enter webId">
+                </FormControl>
+            <Button variant="outline-primary"onClick={()=> console.log(this.state.enteredWebId, this.props.webId, this.updateFriendWebId)}>
+            </Button>
+                <h1>Lista de amigos:</h1>
             {<List src="user.friends">
-              {(friend) => <FriendCardComponent key={`${friend}`} friend={`${friend}`} enable={true}/>}
+                
             </List>}
             </div>
+            </Container>
         );
     }
 }

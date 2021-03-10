1af39a94f217f00c2e998a47de2897f5a5c0f29e
Víctor Suárez Sierra <78354682+uo258606@users.noreply.github.com>
2021-03-10 20:09:46 +0100

    Revert "Mapa añadido"

--- webapp/src/components/AutoSaveSpinner/auto-save.style.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const AutoSaveSpinnerWrapper = styled.span`
-  margin-left: 4px;
-`;
--- webapp/src/containers/index.js
+++
@@ -1,7 +0,0 @@
-import Login from './Login';
-import Register, { RegistrationSuccess } from './Register';
-import PageNotFound from './PageNotFound';
-import Welcome from './Welcome';
-import MyFriends from './MyFriends';
-
-export { Login, Register, RegistrationSuccess, PageNotFound, Welcome, MyFriends };

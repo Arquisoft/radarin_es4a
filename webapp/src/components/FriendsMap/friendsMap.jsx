83e9aff38f87d4cce7c0b462969f66ccef7503b2
Israel <israelmnrg1b@gmail.com>
2021-03-12 16:07:08 +0100

    intentando integración (falta acabar)

--- webapp/src/components/FriendsMap/friendsMap.jsx
+++ webapp/src/components/FriendsMap/friendsMap.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { GoogleApiWrapper, Map, Marker, InfoWindow } from "google-maps-react";
+import {useWebId, List} from  '@solid/react';
+import { useLDflexValue, useLDflexList } from '@solid/react';
+import axios from 'axios'; 
 
 //import './friendsMap.css'
 
 export class FriendsMap extends React.Component {
-  state = { userLocation: { lat: 32, lng: 32 }, loading: true };
+  state = { userLocation: { lat: 32, lng: 32 }, loading: true, webId: "", friendUserList: [] };
 
   componentDidMount(props) {
     navigator.geolocation.getCurrentPosition(
@@ -20,8 +23,27 @@ export class FriendsMap extends React.Component {
         this.setState({ loading: false });
       }
     );
+    //const friends = useLDflexList('user.friends');
+    this.guardarUbicacionUsuarioEnBBDD(this.state.userLocation.lat,this.state.userLocation.lng);
   }
 
+  guardarUbicacionUsuarioEnBBDD(latitudeUser, longitudeUser){
+    var infoUser = 
+    {
+      "webid" : useWebId(), 
+      "data" : { 
+          "friends" : <List src="user.friends"></List>, 
+          "last_location": {
+              "lat": latitudeUser, 
+              "lon": longitudeUser, 
+              "timestamp" : Date.now()
+          } 
+      } 
+  }
+  axios.post(`https://localhost:8888`, infoUser).then(res => {console.log(res);console.log(res.data);})
+  }
+
+  
   render() {
     const { loading, userLocation } = this.state;
     const { google } = this.props;

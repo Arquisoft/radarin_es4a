35d732554787869a699944244ad5b50de7e6fba8
Ramon <ramon.vilafer@gmail.com>
2021-04-07 11:41:59 +0200

    Reset master branch with develop

---
+++ webapp/src/containers/index.js
@@ -0,0 +1,7 @@
+import Login from './Login';
+import Register, { RegistrationSuccess } from './Register';
+import PageNotFound from './PageNotFound';
+import Welcome from './Welcome';
+import MyFriends from './MyFriends';
+
+export { Login, Register, RegistrationSuccess, PageNotFound, Welcome, MyFriends };

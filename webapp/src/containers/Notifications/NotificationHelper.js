b58140dcd32e1e929dfac68f5d6e36c16bc4f5b1
uo271397 <uo271397@uniovi.es>
2021-04-08 19:07:32 +0200

    Funcionando envío de notificaciones a una lista de amigos
    Por ahora la lista de amigos está hardcodeada

--- webapp/src/containers/Notifications/NotificationHelper.js
+++ webapp/src/containers/Notifications/NotificationHelper.js
@@ -3,7 +3,7 @@ import { NotificationTypes, useNotification } from "@inrupt/solid-react-componen
 import { notification } from "@utils";
 import auth from "solid-auth-client";
 import { FriendsList } from "../MyFriends/myfriends.style";
-import { List } from "@solid/react";
+import { List, useLDflexList } from "@solid/react";
 import { getUrl, getUserName } from "../MyFriends/myFriends.component";
 import { sharing } from "../../utils/permissions";
 import { Modal, Button } from "react-bootstrap";
@@ -14,13 +14,16 @@ const Notifications = ({show, setshow }) => {
 
 	const { createNotification } = useNotification(cadena);
 
-	//useEffect(() => {
-	//	auth.trackSession((session) => {
-	//		if (session) {
-				cadena = "https://uo271397.inrupt.net/profile/card#me";
-	//		}
-	//	});
-	//});
+	useEffect(() => {
+		auth.trackSession((session) => {
+			if (session) {
+				cadena = session.webId; 
+			}
+		});
+	});
+
+    //console.log(cadena);
+    //console.log(useLDflexList( "[" + cadena + "].friends" ));
 
 	async function sendNotification(content, to, type, license) {
 		try {
@@ -76,15 +79,16 @@ const Notifications = ({show, setshow }) => {
 
 	function shareWithFriends(e) {
 		e.preventDefault();
+        var friendsList = ["https://uo271397.inrupt.net/profile/card#me", "https://cuartasfabio.inrupt.net/profile/card#me"];
+		for (var key of friendsList) { 
+			showNotifications(key, e);
+		}
         
-		//for (var [ key ] of checkedItems) {
-			showNotifications(cadena, e);
-		//}
 		//givePermissions(checkedItems);
 		setshow(!show);
 	}
 
-	//function givePermissions() {
+	//function givePermissions() { PUEDE QUE NO SIRVA PARA NADA
 	//	let nameRoute = getUrl(cadena) + "private/viade/routes/" + ruta + ".ttl";
 	//	sharing(cadena, checkedItems, nameRoute);
 	//}

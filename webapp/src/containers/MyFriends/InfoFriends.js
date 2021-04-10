a00f70968e3f27c0ac72a3961d2b756ecb4fc1c5
cuartasfabio <fabiocuartaspuente@gmail.com>
2021-04-10 13:46:29 +0200

    Parámetros notificaciones actualizados + cambios toaster

--- webapp/src/containers/MyFriends/InfoFriends.js
+++ webapp/src/containers/MyFriends/InfoFriends.js
@@ -11,7 +11,7 @@ const InfoFriends = (props) => {
 			<div id="itemAmigo">
 					<h3 data-testid="friendId">{name}</h3>
                     <h3 data-testid="friendweb">{webidFriend}</h3>
-					<Notifications mensaje=" quiere quedar contigo." nombreBoton="Quedar" amigo={webidFriend}/>		
+					<Notifications mensaje=" quiere quedar contigo." nombreBoton="Quedar" amigos={[webidFriend]} toastermsg="Amigo notificado con éxito"/>		
             </div>          
 		</RouteCard>
 	);

138ba57caa34dc0c936f17bf0213edb3987ab7f7
Ramon <ramon.vilafer@gmail.com>
2021-04-11 16:23:02 +0200

    Terminada integración de la API con BD. Arreglado despliegue con docker-compose y Heroku.

--- restapi/server.js
+++ restapi/server.js
@@ -1,27 +1,34 @@
 const express = require("express")
 const promBundle = require("express-prom-bundle");
 const cors = require('cors');
-const api = require("./api") 
+const api = require("./api");
 const db = require("./database");
 
-async function connect(){
+function connect(){
+    //The MONGO_URI variable is the connection string to MongoDB Atlas (for production). This env variable is created in heroku.
+    const mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017";
+
+    // Escuchar peticiones en puerto 5000 o según variable de entorno
+    const server_port = process.env.PORT || 5000;
+
+    // Inicializar la BD
+    db.init( mongo_uri );
+
+    // Cargar FW Express
     const app = express()
 
-    // Monitoring middleware
+    //Monitoring middleware
     const metricsMiddleware = promBundle({includeMethod: true});
     app.use(metricsMiddleware);
 
     app.use(cors());
     app.options('*', cors());
     app.use(express.json())
+
+    // Anteponer /api a todas las llamadas
     app.use("/api", api)
 
-
-    app.listen(process.env.PORT || 5000, () => {
-        console.log("Server has started! Using db")
-    })
-
-    db.init();
+    app.listen(server_port, () => console.log("Servidor iniciado. Escuchando en " + server_port))
 }
 
-connect();
+setTimeout(connect, 5000)

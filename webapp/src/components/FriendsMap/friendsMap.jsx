378cd35dd15d11a336f4f5376b22516da266f0b0
Ramon <ramon.vilafer@gmail.com>
2021-04-13 11:53:27 +0200

    Hotfix friendsMap

--- webapp/src/components/FriendsMap/friendsMap.jsx
+++ webapp/src/components/FriendsMap/friendsMap.jsx
@@ -2,22 +2,36 @@ import React, { useEffect, useState } from "react";
 import GoogleMapReact from 'google-map-react';
 import { usePosition } from 'use-position';
 
-/*
 import solid from '@solid/query-ldflex';
-*/
 
-import { useWebId } from  '@solid/react';
-import { useLDflexList } from '@solid/react';
+import { useWebId, List } from  '@solid/react';
+import { useLDflexValue, useLDflexList } from '@solid/react';
 import axios from 'axios'; 
 
 //import './friendsMap.css'
 
 
-var amigos = [];
+var veces = 0;
 
 const Marker = (props) => (
   <div style={{
     color: 'white', 
+    background: props.color,
+    padding: '15px 15px',
+    display: 'inline-flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    transform: 'translate(-50%, -50%)'
+  }}>
+    {props.text}
+  </div>
+);
+
+const Circle = (props) => (
+  <div style={{
+    color: 'red', 
     background: props.color,
     padding: '15px 15px',
     display: 'inline-flex',
@@ -43,22 +57,26 @@ const radius = () => {
 
 console.log(radius());
 
-/*
 async function getUserWebID () {
   const user = solid.data.user;
   const webID = await user;
   return webID;
 }
-*/
 
 async function getFriends( friends ) {
   const friendsValue = await friends;
   return friendsValue;
 }
 
+
+
+
 // Use React.Memo
 function FriendsMap( props ) {
   const [userFriendsList, setUserFriendsList] = useState( [] );
+  const [serverResponse, setServerResponse] = useState( {} );
+  const [userWebID, setUserWebID] = useState( useWebId() );
+  const [lista, setLista] = useState( {} );
 
   const tempFriendsList = [];
   const webID = useWebId();
@@ -66,31 +84,35 @@ function FriendsMap( props ) {
   const { latitude, longitude } = usePosition( false );
 
   getFriends(useLDflexList( "[" + webID + "].friends" ))
-      .then( (friendsList) => { setUserFriendsList( friendsList ) });
+        .then( (friendsList) => { setUserFriendsList( friendsList ) });
 
-  useEffect( () => {
-    userFriendsList.forEach(element => tempFriendsList.push( element.valueOf() ) );
 
-    let userInfo = { 
-      "webid": webID,
-      "data": {
-        "friends": tempFriendsList, 
-        "last_location": {
-          "lat": latitude, 
-          "lon": longitude, 
-          "timestamp": Date.now() 
+    //Metemos esto en una función para que no se ejecute todo el rato
+    function prueba(){
+      userFriendsList.forEach(element => tempFriendsList.push( element.valueOf() ) );
+
+      let userInfo = { 
+        "webid": webID,
+        "data": {
+          "friends": tempFriendsList, 
+          "last_location": {
+            "lat": latitude, 
+            "lon": longitude, 
+            "timestamp": Date.now() 
+          }
         }
+      }
+    
+      if (tempFriendsList.length > 0 ){
+        const apiEndPoint = process.env.REACT_APP_API_URI || "http://localhost:5000/api";
+        axios.post( apiEndPoint + '/users/update', userInfo )
+            .then(res => { setLista(res.data) }); //Usamos el setState y metemos la lista que luego usaremos con setList
       }
     }
 
-    const apiEndPoint = process.env.REACT_APP_API_URI || "http://localhost:5000/api";
-    if (tempFriendsList.length > 0 ){
-      axios.post( apiEndPoint + "/users/update", userInfo )
-          .then(res => {console.log("Request complete! response:", res.data ); amigos.push(res.data) });
-    }
-    
-  }, [userFriendsList] );
-  
+    // TODO: Fix
+    if ( veces < 1000 ) { prueba(); veces++; }
+
   return (
     <div style={{ height: '80vh', width: '100%' }}>
       <GoogleMapReact
@@ -99,6 +121,7 @@ function FriendsMap( props ) {
       defaultZoom={15}
       yesIWantToUseGoogleMapApiInternals={true}
       onGoogleApiLoaded={({map, maps}) =>
+      {
         new maps.Circle({
           strokeColor: '#FF0000',
           strokeOpacity: 0.8,
@@ -108,9 +131,18 @@ function FriendsMap( props ) {
           map,
           center: {lat: latitude, lng: longitude},
           radius: radius()*1000,
-        })}
+        });
+      }}
         >
        <Marker lat={ latitude } lng={ longitude } color="blue" text="Tú"/>
+
+       { Object.keys(lista).map( (amigo) => {
+          return (
+            <Marker lat={ lista[amigo].lat } lng={ lista[amigo].lon } color="green" text={ amigo.split("/")[2].split(".")[0] } />
+          );
+       })}
+      
+       
     </GoogleMapReact> 
     </div>
   );

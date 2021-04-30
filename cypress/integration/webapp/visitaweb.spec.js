30c95c629edcfec2aa461b2eda1bcefa7d8baba9
uo251950 <uo251950@uniovi.es>
2021-05-01 01:47:43 +0200

    Cambio visitaweb

--- cypress/integration/webapp/visitaweb.spec.js
+++ cypress/integration/webapp/visitaweb.spec.js
@@ -4,7 +4,6 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-/// <reference types="cypress" />
 describe('My First Test', () => {
     it('Does not do much!', () => {
         cy.visit('https://radarines4awebapp.herokuapp.com')

02b20a149ec186588adb4394bde4efb0a802e634
uo251950 <uo251950@uniovi.es>
2021-05-01 01:40:48 +0200

    Probando Cypress

---
+++ cypress/integration/webapp/visitaweb.spec.js
@@ -0,0 +1,12 @@
+// untitled.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+/// <reference types="cypress" />
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+        cy.visit('https://radarines4awebapp.herokuapp.com')
+    })
+})

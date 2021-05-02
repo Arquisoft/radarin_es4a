c8a06a1f9ca44101c06a7f3aaa49286caa41e43f
Víctor Suárez Sierra <uo258606@uniovi.es>
2021-05-02 13:40:15 +0200

    Añadida librería

--- webapp/test/__mocks__/@solid/query-ldflex.js
+++ webapp/test/__mocks__/@solid/query-ldflex.js
@@ -1,5 +1,6 @@
 const { context } = jest.requireActual("@solid/query-ldflex").default;
-
+//@solid/query-ldflex
+//webapp/node_modules/@solid/query-ldflex
 export default {
   context,
   resolve: jest.fn(),

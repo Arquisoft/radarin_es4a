5c0a6220fedb103c5bf4d9981c793c6f01ad1ad3
uo271397 <uo271397@uniovi.es>
2021-05-02 21:54:51 +0200

    Subiendo un poco el codecov

--- webapp/test/__mocks__/@solid/query-ldflex.js
+++ webapp/test/__mocks__/@solid/query-ldflex.js
@@ -1,6 +1,5 @@
 const { context } = jest.requireActual("@solid/query-ldflex").default;
-//@solid/query-ldflex
-//webapp/node_modules/@solid/query-ldflex
+
 export default {
   context,
   resolve: jest.fn(),

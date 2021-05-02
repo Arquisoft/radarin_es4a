132d2814e8b2b6abca790ed9c83d858ef0d19b35
uo271397 <uo271397@uniovi.es>
2021-05-03 00:23:53 +0200

    Solucionando problema de tests y añadiendo algún test

--- webapp/test/__mocks__/@solid/query-ldflex.js
+++ webapp/test/__mocks__/@solid/query-ldflex.js
@@ -1,7 +1,6 @@
-const { context } = jest.requireActual("@solid/query-ldflex").default;
+//const { context } = jest.requireActual("@solid/query-ldflex").default;
 
 export default {
-  context,
   resolve: jest.fn(),
   clearCache: jest.fn()
 };
